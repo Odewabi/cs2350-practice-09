05f5946f30098d6d564bcd0545dc0472d134d3e0
Odewabi <odewabitomisin43@gmail.com>
2023-07-14 12:06:24 -0600

    Final submission

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,6 +1,11 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import 'bootstrap'
 
+
+
+
+
+
 import { movies } from './movies'
 
 for (let m of movies) {
@@ -13,6 +18,7 @@ for (let m of movies) {
         displayMovie(m)
     }
 }
+
 let featured_movie = document.querySelector(".featured")
 function displayMovie(movie) {
     featured_movie.innerHTML = `
@@ -23,7 +29,7 @@ function displayMovie(movie) {
                 <h5 class="card-title"><small>${movie.year}, ${movie.genre}</small></h5>
                 <p class="card-text">${movie.plot}</p>
             </div> 
-            <div class="card-footer text-body-secondary">
+            <div class="card-footer text-muted">
                 <div class="row row-cols-3">
                     <div class="col"><strong>Rating: </strong>${movie.rating}</div>
                     <div class="col"><strong>Rated: </strong>${movie.rated}</div>
@@ -35,15 +41,15 @@ function displayMovie(movie) {
 } 
 
 function searchMovies(event){
-    event.preventDeafault()
+    event.preventDefault()
 
-    let input = document.querySelector('[type="serch"]').value || ""
+    let input = document.querySelector('[type="search"]').value || ""
     let count = 0
     for (let m of movies){
         if(m.title.toUpperCase().indexOf(input.toUpperCase()) == -1){
             document.querySelector(`#m${m.id}`).classList.add('d-none')
         }else{
-            document.querySelector(`#m${m.id}`).classList.add('d-none')
+            document.querySelector(`#m${m.id}`).classList.remove('d-none')
             count++
         }
     }
@@ -51,6 +57,6 @@ function searchMovies(event){
     featured_movie.innerHTML = count == 0 ? 'Nothing was found' : ''
 }
 
-document.querySelector("button".onclick) = searchMovies
+document.querySelector("button").onclick = searchMovies
 document.querySelector('[type="search"]').onsearch = searchMovies
 document.querySelector("form").onsubmit = searchMovies
